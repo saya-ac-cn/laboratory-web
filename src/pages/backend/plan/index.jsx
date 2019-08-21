534d586810a1c239e06f5d14431e6d5e4f216f17
liunengkai <saya@saya.ac.cn>
2019-08-21 23:14:53 +0800

    完成计划列表数据展示及修改页面弹出

--- src/pages/backend/plan/index.jsx
+++ src/pages/backend/plan/index.jsx
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Button, Col, DatePicker, Icon, Form, Row} from "antd";
+import {Button, Col, DatePicker, Icon, Form, Row, Modal, Input} from "antd";
 import DocumentTitle from 'react-document-title'
 import './index.less'
 import {getPlanList, createPlan, updatePlan, deletePlan} from "../../../api";
 import {openNotificationWithIcon} from "../../../utils/window";
+import PropTypes from "prop-types";
 import moment from 'moment';
 /*
  * 文件名：index.jsx
@@ -18,9 +19,17 @@ class Plan extends Component {
     state = {
         listLoading: false,
         datas:[],
+        outhtml:[],
         filters: {
             date: ""
-        }
+        },
+        formVisible: false,
+        //编辑界面数据
+        editForm: {
+            id: '',
+            planDate: '',
+            planContent: '',
+        },
     }
 
     /**
@@ -29,8 +38,9 @@ class Plan extends Component {
      */
     getDatas = async () => {
         let _this = this
+        let filters = _this.state.filters
         let para = {
-            date: _this.state.filters.date,
+            date: filters.date,
         };
         // 在发请求前, 显示loading
         _this.setState({listLoading: true});
@@ -83,23 +93,25 @@ class Plan extends Component {
                     // 没有安排计划
                     // 判断当前单元格是否是今天
                     if(isNowMonth === true && nowday === item.number){
-                        _thisLine.push(<td key={item.id} data-id={item.id} data-key={editDate+item.number} className="today">{item.number}</td>)
+                        _thisLine.push(<td key={i} onClick={this.clickTD} data-id={item.id} data-key={editDate+item.number} className="today">{item.number}</td>)
                     }else {
-                        _thisLine.push(<td key={item.id} data-id={item.id} data-key={editDate+item.number}>{item.number}</td>)
+                        _thisLine.push(<td key={i} onClick={this.clickTD} data-id={item.id} data-key={editDate+item.number}>{item.number}</td>)
                     }
                 }else{
                     // 有计划
-                    _thisLine.push(<td key={item.id} className="havetoday" data-id={item.id} data-key={editDate+item.number}>{item.number}</td>)
+                    _thisLine.push(<td key={i} onClick={this.clickTD} className="havetoday" data-id={item.id} data-key={editDate+item.number} data-value={item.value}>{item.number}</td>)
                 }
             }else{
                 // 显示1号前和月尾的空白单元格
-                _thisLine.push(<td key={item.id}></td>)
+                _thisLine.push(<td key={i}></td>)
             }
             if(cellNum === 6){
                 outhtml.push(<tr key={lineNum}>{_thisLine}</tr>)
             }
         }
-        console.log(outhtml);
+        this.setState({
+            outhtml
+        })
     };
 
     /**
@@ -169,11 +181,60 @@ class Plan extends Component {
         let _this = this
         // 通过上一个月，下一个月进行日期查询
         let filters = _this.state.filters
-        filters.date = this.getOperationData(new Date(filters.date),flog)
-        _this.setState({
-            filters
-        },function () {
+        filters.date = _this.getOperationData(new Date(filters.date),flog)
+        _this.setState({filters},function () {
             _this.getDatas()
+        })
+    };
+
+    /**
+     * 单击单元格事件
+     * @param e
+     */
+    clickTD = (e) => {
+        let _this = this;
+        // 得到自定义属性
+        // 得到计划的主键，没有计划时为-1
+        let id =  e.currentTarget.getAttribute('data-id')
+        // 得到当天的时间
+        let key =  e.currentTarget.getAttribute('data-key')
+        let {formVisible, editForm} = _this.state
+        formVisible = true
+        if (id === -1 || id === '-1'){
+            // 该天无计划
+            editForm.planDate = key
+        } else {
+            // 该天有计划
+            let value =  e.currentTarget.getAttribute('data-value')
+            editForm.planContent = value
+            editForm.planDate = key
+            editForm.id = id
+        }
+        _this.setState({
+            formVisible:formVisible,
+            editForm:editForm
+        })
+    };
+
+    /**
+     * 取消上传
+     */
+    handleCloseForm = () => {
+        let _this = this;
+        let formVisible = false;
+        _this.setState({
+            formVisible:formVisible
+        })
+    };
+
+    /**
+     * 打开上传框
+     */
+    handleSubmitForm = () => {
+        let _this = this;
+        let formVisible = true;
+        _this.setState({
+            formVisible
         })
     };
 
@@ -187,7 +248,10 @@ class Plan extends Component {
         this.setState({
             filters
         })
-        console.log(filters)
+        this.formItemLayout = {
+            labelCol: {span: 4},
+            wrapperCol: {span: 14},
+        };
     }
 
     /*
@@ -200,10 +264,40 @@ class Plan extends Component {
 
 
     render() {
+        const {formVisible, editForm} = this.state;
         const {date} = this.state.filters;
+        const outhtml = this.state.outhtml
+        const {getFieldDecorator} = this.props.form;
         return (
             <DocumentTitle title="日程安排">
                 <section>
+                    <Modal
+                        title="计划安排"
+                        width="50%"
+                        visible={formVisible === true}
+                        okText='提交'
+                        onOk={this.handleSubmitForm}
+                        onCancel={this.handleCloseForm}>
+                        <Form {...this.formItemLayout}>
+                            <Form.Item label="计划时间" {...this.formItemLayout}>
+                                <Input disabled={true} value={editForm.planDate}/>
+                            </Form.Item>
+                            <Form.Item label="计划内容：" {...this.formItemLayout}>
+                                {
+                                    getFieldDecorator('reply', {
+                                        initialValue: editForm.planContent,
+                                        rules: [
+                                            {required: true, message: '请输入计划内容'},
+                                            {min: 1, message: '长度在 1 到 50 个字符'},
+                                            {max: 140, message: '长度在 1 到 140 个字符'},
+                                        ]
+                                    })(
+                                        <Input.TextArea rows={4} placeholder='请输入计划内容'/>
+                                    )
+                                }
+                            </Form.Item>
+                        </Form>
+                    </Modal>
                     <Row>
                         <Col span={24} className="toolbar">
                             <Form layout="inline">
@@ -222,15 +316,15 @@ class Plan extends Component {
                         <Col span={24}>
                             <div
                                 style={{float: 'left',width: '30%',height: '100%',textAlign: 'left',lineHeight: '45px',cursor: 'pointer'}}>
-                                <span><Icon type="left" /></span>
+                                <span onClick={() => this.buttonQuery(-1)}><Icon type="left" /></span>
                             </div>
                             <div
                                 style={{float: 'left',width: '40%',height: '100%',textAlign: 'center',lineHeight: '45px',fontSize: '20px'}}>
-                                909090
+                                {date}
                             </div>
                             <div
                                 style={{float: 'right',width: '28%',height: '100%',textAlign: 'right',lineHeight: '45px',cursor: 'pointer'}}>
-                                <span><Icon type="right" /></span>
+                                <span onClick={() => this.buttonQuery(+1)}><Icon type="right" /></span>
                             </div>
                         </Col>
                     </Row>
@@ -248,6 +342,9 @@ class Plan extends Component {
                                     <td>星期六</td>
                                 </tr>
                                 </thead>
+                                <tbody>
+                                {outhtml}
+                                </tbody>
                             </table>
                         </Col>
                     </Row>
@@ -258,4 +355,5 @@ class Plan extends Component {
 }
 
 // 对外暴露
-export default Plan;
+const WrapPlan = Form.create()(Plan)
+export default WrapPlan;
