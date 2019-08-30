45408828010317d20cf2e63f83067cafd8ba4fc4
liunengkai <saya@saya.ac.cn>
2019-08-30 23:52:58 +0800

    编写动态表单

--- src/pages/backend/financial/addForm.jsx
+++ src/pages/backend/financial/addForm.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types'
-import {Row, Col, Form, Input, Card, Select, Button, Icon} from "antd";
+import {Row, Col, Form, Input, Select, Button, Icon} from "antd";
+import {openNotificationWithIcon} from "../../../utils/window";
 
 /*
  * 文件名：addForm.jsx
@@ -8,6 +9,7 @@ import {Row, Col, Form, Input, Card, Select, Button, Icon} from "antd";
  * 创建日期：2019-08-29 - 21:50
  * 描述：财务申报
  */
+const {Option} = Select;
 
 // 定义组件（ES6）
 class AddForm extends Component {
@@ -22,13 +24,13 @@ class AddForm extends Component {
     };
 
     state = {
-        form:{
-            tradeType:1,
-            transactionAmount:'',
-            infoList:[{
-                flog:2,
-                currencyNumber:0,
-                currencyDetails:''
+        form: {
+            tradeType: 1,
+            transactionAmount: '',
+            infoList: [{
+                flog: 1,
+                currencyNumber: 0,
+                currencyDetails: ''
             }]
         }
     }
@@ -37,14 +39,42 @@ class AddForm extends Component {
     continueAdd = () => {
         let form = this.state.form
         let item = {
-            flog:2,
-            currencyNumber:0,
-            currencyDetails:''
+            flog: 1,
+            currencyNumber: 0,
+            currencyDetails: ''
         }
         form.infoList.push(item);
         this.setState({form})
         return false;
     }
+
+    // 删除明细添加行
+    deleteLine = (index) => {
+        let _this = this
+        let form = _this.state.form
+        console.log(index,form.infoList)
+        if(form.infoList.length <= 1){
+            openNotificationWithIcon("error", "错误提示", '每一笔流水申请下边必须要有一条详情记录');
+        }else{
+            form.infoList.splice(index,1);
+            _this.setState({form},function () {
+                console.log(_this.state.form.infoList)
+            })
+        }
+    }
+
+    /**
+     * 双向绑定用户查询主题
+     * @param event
+     */
+    onChangeDate = (event) => {
+        let _this = this;
+        const value = event.target.value;
+        let form = _this.state.form;
+        let currencyNumber =  event.currentTarget.getAttribute('data-currencynumber')
+        form.infoList[currencyNumber].currencyNumber = value
+        _this.setState({form})
+    };
 
 
     componentWillMount() {
@@ -57,9 +87,10 @@ class AddForm extends Component {
 
 
     render() {
-        const {tradeType,transactionAmount,infoList} = this.state.form
+        const {tradeType, transactionAmount, infoList} = this.state.form
         const {type} = this.props;
-        const { getFieldDecorator } = this.props.form;
+        const {getFieldDecorator} = this.props.form;
+        let _this = this
         return (
             <section>
                 <div className="mytips">
@@ -76,7 +107,7 @@ class AddForm extends Component {
                         </p>
                     </blockquote>
                 </div>
-                <Form {...this.formItemLayout}>
+                <Form {...this.formItemLayout} style={{marginTop: '1.5em'}}>
                     <Form.Item label="交易类别：" {...this.formItemLayout}>
                         {
                             getFieldDecorator('tradeType', {
@@ -85,8 +116,8 @@ class AddForm extends Component {
                                     {required: true, message: '请选择交易类别'}
                                 ]
                             })(
-                                <Select style={{width:'200px'}} showSearch onChange={this.onChangeType}
-                                        allowClear={true}  placeholder="请选择交易类别">
+                                <Select style={{width: '200px'}} showSearch onChange={this.onChangeType}
+                                        allowClear={true} placeholder="请选择交易类别">
                                     {type}
                                 </Select>
                             )
@@ -107,54 +138,78 @@ class AddForm extends Component {
                         }
                     </Form.Item>
                 </Form>
-                <Row style={{display: 'flex',alignItems: 'center',justifyItems: 'center',marginBottom:'1em'}}>
-                    <Col span={4} style={{textAlign:'right',display: 'flex',alignContent: 'center',justifyContent: 'flex-end'}}>
-                        <div>流水1：</div>
-                    </Col>
-                    <Col span={18}>
-                        <div>
-                            <Form layout="inline">
-                                <Form.Item>
-                                    {getFieldDecorator('username', {
-                                        rules: [{ required: true, message: 'Please input your username!' }],
-                                    })(
-                                        <Input
-                                            prefix={<Icon type="user" />}
-                                            placeholder="Username"
-                                        />,
-                                    )}
-                                </Form.Item>
-                                <Form.Item>
-                                    {getFieldDecorator('password', {
-                                        rules: [{ required: true, message: 'Please input your Password!' }],
-                                    })(
-                                        <Input
-                                            prefix={<Icon type="lock" />}
-                                            type="password"
-                                            placeholder="Password"
-                                        />,
-                                    )}
-                                </Form.Item>
-                                <Form.Item>
-                                    {getFieldDecorator('password2', {
-                                        rules: [{ required: true, message: 'Please input your Password!' }],
-                                    })(
-                                        <Input
-                                            prefix={<Icon type="lock" />}
-                                            type="password"
-                                            placeholder="Password"
-                                        />,
-                                    )}
-                                </Form.Item>
-                                <Form.Item>
-                                    <Button type="primary" htmlType="submit">
-                                        Log in
-                                    </Button>
-                                </Form.Item>
-                            </Form>
-                        </div>
-                    </Col>
-                </Row>
+                {
+                    infoList.map(function (item,index) {
+                        return (
+                            <Row key={index} style={{display: 'flex', alignItems: 'center', justifyItems: 'center', marginBottom: '1em'}}>
+                                <Col span={4} style={{
+                                    textAlign: 'right',
+                                    display: 'flex',
+                                    alignContent: 'center',
+                                    justifyContent: 'flex-end'
+                                }}>
+                                    <div>流水{(index+1)}：</div>
+                                </Col>
+                                <Col span={18}>
+                                    <div>
+                                        <Form layout="inline">
+                                            <Form.Item>
+                                                {getFieldDecorator("flog"+index, {
+                                                    initialValue: item.flog+'',
+                                                    rules: [{required: true, message: '请选择出入方式'}],
+                                                })(
+                                                    <Select suffixIcon={<Icon type="select"/>}
+                                                            style={{width: 100}}>
+                                                        <Option value="1">存入</Option>
+                                                        <Option value="2">取出</Option>
+                                                    </Select>
+                                                )}
+                                            </Form.Item>
+                                            <Form.Item>
+                                                {getFieldDecorator('currencyNumber'+index, {
+                                                    initialValue: item.currencyNumber,
+
+                                                    rules: [
+                                                        {required: true, message: '请填写金额'},
+                                                        {max: 12, message: '长度在 12个字符以内'},
+                                                    ],
+                                                })(
+                                                    <Input
+                                                        prefix={<Icon type="pay-circle"/>}
+                                                        type="number"
+                                                        data-currencynumber={index}
+                                                        onChange={_this.onChangeDate}
+                                                        placeholder="金额"
+                                                    />,
+                                                )}
+                                            </Form.Item>
+                                            <Form.Item>
+                                                {getFieldDecorator('currencyDetails'+index, {
+                                                    rules: [
+                                                        {required: true, message: '请填写摘要'},
+                                                        {min: 2, message: '长度在 2 到 20 个字符'},
+                                                        {max: 20, message: '长度在 2 到 20 个字符'},
+                                                    ],
+                                                })(
+                                                    <Input
+                                                        prefix={<Icon type="account-book"/>}
+                                                        type="text"
+                                                        placeholder="说明"
+                                                    />,
+                                                )}
+                                            </Form.Item>
+                                            <Form.Item>
+                                                <Button type="primary" htmlType="button" onClick={()=>_this.deleteLine(index)}>
+                                                    删除{index}
+                                                </Button>
+                                            </Form.Item>
+                                        </Form>
+                                    </div>
+                                </Col>
+                            </Row>
+                        )
+                    })
+                }
                 <Row>
                     <Col offset={4} span={18}>
                         <Button type="primary" htmlType="button" onClick={this.continueAdd}>
