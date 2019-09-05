dc0e0df7c3e1066248c2d35595688239ef96be55
liunengkai <saya@saya.ac.cn>
2019-09-05 22:43:01 +0800

    增加后台管理模板

--- src/pages/layout/backend2/index.jsx
+++ src/pages/layout/backend2/index.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Button,Icon } from 'antd';
 import './index.less'
 /*
  * 文件名：index.jsx
@@ -11,22 +12,54 @@ import './index.less'
 class Admin extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            collapsed: false,
+        };
     }
 
+    // 切换面板
+    handlTabClick = () => {
+        const collapsed = !this.state.collapsed;
+        // 更新状态
+        this.setState({collapsed: collapsed})
+    };
 
     render() {
+        // 读取状态
+        const {collapsed} = this.state;
         return (
             <div className="this-container">
-                <div className='background-div'>
-                    <img style={{backgroundImage:'url('+process.env.PUBLIC_URL+"/picture/login/login_background2.jpg"+')'}}/>
+                <div className='background-div' style={{backgroundImage:'url('+process.env.PUBLIC_URL+"/picture/backend/admin_background1.jpg"+')'}}>
                 </div>
-                <header className="this-header">头部</header>
+                <header className="this-header">
+                    <div className='header-logo'>
+                        <Button type="link" ghost onClick={this.handlTabClick}>
+                            <Icon type='menu'/>
+                        </Button>
+                    </div>
+                    <div className='header-search'>搜索</div>
+                    <div className='header-info'>退出</div>
+                </header>
                 <section className="this-content">
-                    <div className='leftmunu-open'></div>
-                    <div className='content-container'></div>
+                    <div className={`leftmunu ${collapsed ? 'leftmunu-close' : 'leftmunu-open'}`}>
+                        <div className='menu-logo'>快速入口</div>
+                        <div className='menu-list'>菜单列表</div>
+                        <div className='menu-copyright'>版权</div>
+                    </div>
+                    <div className='content-container'>
+                        <div className='pagename-div'>
+                            <div className='pagename-label'>页面路径:用户中心/修改密码</div>
+                        </div>
+                        <div className='content-div'>
+                            <div className='container-div'>
+                                无数据
+                            </div>
+                            <div className='operation-info'>上次操作明细</div>
+                        </div>
+                    </div>
+                    <div className='quick-div'>2</div>
                 </section>
-                <footer className="this-footer"></footer>
             </div>
         );
     }
