7531563d81a19472604935803a09d9bf3ab83e55
liunengkai <saya@saya.ac.cn>
2019-09-06 23:33:40 +0800

    完善后台管理模板

--- src/pages/layout/backend2/index.jsx
+++ src/pages/layout/backend2/index.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Button,Icon } from 'antd';
+import {Input, Button, Icon, Dropdown, Menu, Col} from 'antd';
 import './index.less'
 /*
  * 文件名：index.jsx
@@ -7,7 +7,25 @@ import './index.less'
  * 创建日期：2019-09-04 - 22:15
  * 描述：管理页面
  */
-
+const menu = (
+    <Menu>
+        <Menu.Item key="0">
+            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                1st menu item
+            </a>
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="1">
+            <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                2nd menu item
+            </a>
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="3">
+            3rd menu item（disabled）
+        </Menu.Item>
+    </Menu>
+);
 // 定义组件（ES6）
 class Admin extends Component {
 
@@ -29,23 +47,54 @@ class Admin extends Component {
         // 读取状态
         const {collapsed} = this.state;
         return (
-            <div className="this-container">
+            <div className="backend-container">
                 <div className='background-div' style={{backgroundImage:'url('+process.env.PUBLIC_URL+"/picture/backend/admin_background1.jpg"+')'}}>
                 </div>
                 <header className="this-header">
                     <div className='header-logo'>
-                        <Button type="link" ghost onClick={this.handlTabClick}>
-                            <Icon type='menu'/>
-                        </Button>
+                        <div className='tab-operation'>
+                            <Button type="link" size='large' onClick={this.handlTabClick}>
+                                <Icon type='menu'/>
+                            </Button>
+                        </div>
+                        <div className='project-div' style={{backgroundImage:'url('+process.env.PUBLIC_URL+"/picture/svg/project.svg"+')'}}>
+                        </div>
+                        <div className='project-name'>
+                            Lab
+                        </div>
                     </div>
-                    <div className='header-search'>搜索</div>
-                    <div className='header-info'>退出</div>
+                    <div className='header-search'>
+                        <div className='header-search-form'>
+                            <div className='header-search-form-input'>
+                                <Input
+                                    prefix={<Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="搜索笔记" style={{width:'100%',height:'100%'}}
+                                />
+                            </div>
+                        </div>
+                        <div className='header-search-menu'>
+                            <Dropdown overlay={menu}>
+                                <Icon type="appstore" />
+                            </Dropdown>
+                        </div>
+                    </div>
+                    <div className='header-info'>
+                        <Dropdown overlay={menu}>
+                          <span className="el-dropdown-link userinfo-inner">
+                            <img src='https://saya.ac.cn/files/picture/logo/Pandora/20190715/2019071598184.png' alt="logo"/>
+                          </span>
+                        </Dropdown>
+                    </div>
                 </header>
                 <section className="this-content">
                     <div className={`leftmunu ${collapsed ? 'leftmunu-close' : 'leftmunu-open'}`}>
-                        <div className='menu-logo'>快速入口</div>
+                        <div className='menu-logo'>
+                            <div className={`logo-item ${collapsed?"menu-logo-close":null}`}>
+                                写笔记
+                            </div>
+                        </div>
                         <div className='menu-list'>菜单列表</div>
-                        <div className='menu-copyright'>版权</div>
+                        <div className={`menu-copyright ${collapsed?"menu-copyright-close":null}`}>版权</div>
                     </div>
                     <div className='content-container'>
                         <div className='pagename-div'>
